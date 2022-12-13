3b4f0f5441dd3b9f75c261fba39abdab1aa1bddd
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2022-12-13 19:12:05 +0100

    working on kata

---
+++ kata.js
@@ -0,0 +1,24 @@
+function zero(operand) {
+  return 0 ;
+}
+
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+
+function plus() {}
+function minus() {}
+function times() {
+  return function (x){
+    return x* operand;
+  }
+}
+function dividedBy() {}
+
+zero();
